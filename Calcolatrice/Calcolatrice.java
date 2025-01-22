d679d4bf1544c1494929f81de9f0a0be33d1a893
ThisDavide <sdavide.policelli@itis.pr.it>
2025-01-22 18:24:42 +0100

    tentativo algoritmo per calcolo risultato

--- Calcolatrice/Calcolatrice.java
+++ Calcolatrice/Calcolatrice.java
@@ -6,8 +6,8 @@ import java.awt.Font;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.*;
-
 import javax.swing.*;
+
 
 public class Calcolatrice extends JFrame implements ActionListener{
     JButton[] arrTasti;
@@ -20,7 +20,7 @@ public class Calcolatrice extends JFrame implements ActionListener{
     }
 
     public void creaCalcolatrice(){
-        arrSimboli = new char[15];
+        arrSimboli = new char[18];
         arrSimboli[0] = '1';
         arrSimboli[1] = '2';
         arrSimboli[2] = '+';
@@ -36,11 +36,14 @@ public class Calcolatrice extends JFrame implements ActionListener{
         arrSimboli[12] = '9';
         arrSimboli[13] = '0';
         arrSimboli[14] = '=';
+        arrSimboli[15] = ' ';
+        arrSimboli[16] = 'C';
+        arrSimboli[17] = 'e';
 
         JPanel pannelloTasti = new JPanel(); //pannello per i tasti della calcolatrice
         pannelloTasti.setBackground(Color.black);
         this.add(pannelloTasti, BorderLayout.CENTER);
-        pannelloTasti.setLayout(new GridLayout(5, 3)); 
+        pannelloTasti.setLayout(new GridLayout(6, 3)); 
 
         JPanel pannelloRis = new JPanel(); //pannello dove andrà il label per il risultato
         pannelloRis.setPreferredSize(new Dimension(400, 100));
@@ -48,13 +51,13 @@ public class Calcolatrice extends JFrame implements ActionListener{
         this.add(pannelloRis, BorderLayout.NORTH);
         pannelloRis.setLayout((LayoutManager) new FlowLayout(FlowLayout.RIGHT));  // Allinea a destra
 
-        JLabel label = new JLabel(""); //label per il risultato
-        label.setFont(new Font("Arial", Font.BOLD, 25));
-        label.setForeground(Color.white);
-        pannelloRis.add(label);
+        labelRisultato = new JLabel(""); //label per il risultato
+        labelRisultato.setFont(new Font("Arial", Font.BOLD, 25));
+        labelRisultato.setForeground(Color.white);
+        pannelloRis.add(labelRisultato);
 
-        arrTasti = new JButton[15];
-        for (int i = 0; i < 15; i++){ 
+        arrTasti = new JButton[18];
+        for (int i = 0; i < 18; i++){ 
             for(int j = i; j <= i; j++){
                 arrTasti[i] = new JButton(String.valueOf(arrSimboli[j]));
                 arrTasti[i].setFont(new Font("Arial", Font.BOLD, 25)); //Dimensione carattere
@@ -64,7 +67,7 @@ public class Calcolatrice extends JFrame implements ActionListener{
             }                       
 		}
 
-        for(int i = 0; i < 15; i++){
+        for(int i = 0; i < 18; i++){
             arrTasti[i].setBackground(Color.darkGray);
         }
 
@@ -74,13 +77,104 @@ public class Calcolatrice extends JFrame implements ActionListener{
 		setVisible(true); 
     }
 
+    String txtPrec = "";
+    String[] arrInput = new String[100];
+    int index = 0;  // Indice per l'array di input
+
     public void actionPerformed(ActionEvent e) {
         JButton clickedButton = (JButton)e.getSource();
-        String btnTxt = clickedButton.getText();
+        String btnTxt = clickedButton.getText(); //testo del bottone schiacciato
+
+        if(btnTxt.equals("e")){
+            System.exit(0);
+        }
+
+        if(btnTxt.equals("C")){
+            cancella();
+            txtPrec = "";
+            index = 0; //resetto l'indice
+        }
+        else if(btnTxt.equals("=")){
+            labelRisultato.setText(calcolaRisultato());
+        }
+        else{
+            arrInput[index] = btnTxt; 
+            index++;
+            txtPrec += btnTxt;
+            labelRisultato.setText(txtPrec);        //lo aggiungo nel label del risultato
+        }
     }
 
-    //Metodo per calcolo del risultato
-    public String calcolaRisultato(String input){
-        input.replace(" ", ""); //tolgo gli spazi
+    public void cancella(){
+        labelRisultato.setText("");
+    }
+
+    public String calcolaRisultato(){
+        String input = String.join("", arrInput); //tolgo tutti gli spazi nell'array con gli input
+
+        try {
+            double ris = calcolaVal(input);
+            return String.valueOf(ris); //restituisco il risultato come stringa
+        } catch (Exception e) {
+            return "Errore";
+        }
+    }
+
+    public double calcolaVal(String s) {
+        return 0;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int[] arrNum;
+        arrNum = new int[arrInput.length];
+        char strToChar = 0;
+        int charToInt = 0;
+        int ris = 0;
+
+        for(int i = 0; i < arrInput.length; i++){
+            if(!arrInput[i].equals("+") && !arrInput[i].equals("-") && !arrInput[i].equals("*") && !arrInput[i].equals("/") && !arrInput[i].equals("=")){
+                strToChar = arrInput[i].charAt(0);
+                charToInt = strToChar;
+                arrNum[i] = charToInt;
+            }
+            else{
+                if(arrInput[i].equals("+")){
+                    for(int j = 0; j < arrNum.length; j++){
+                        ris += arrNum[j];
+                    }
+                }
+            }
+        }
+
+
+        char charRis = (char)ris;
+        String strRis = new String(new char[] {charRis});
+
+        return strRis;*/
